af936e3621df4089a6d5b59aff9e222319d9a8e8
Amelinda <amelindaa2705@gmail.com>
2023-12-04 17:21:55 +0700

    add update and delete

---
+++ php-04-12-2023/delete.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once 'koneksi.php';
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    $q = $conn->query("DELETE FROM produk WHERE id_produk = '$id'");
+
+    if ($q) {
+        echo "<script>alert('Data berhasil dihapus'); window.location.href='index.php'</script>";
+    } else {
+        echo "<script>alert('Data tidak berhasil dihapus'); window.location.href='index.php'</script>";
+    }
+} else {
+    header(Location: index.php);
+}
+
+?>
